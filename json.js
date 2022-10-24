46c3d93954614a075a75989baa7dd58e28f8a3d0
rrakib2 <rakib.rupom007@gmail.com>
2022-10-24 10:06:19 +0600

    modified json file

--- json.js
+++ json.js
@@ -1,6 +1,8 @@
 const user = {
     id: 23,
-    name: 'Rakib Talukder Rupom'
+    name: 'Rakib Talukder Rupom',
+    friends: ['rupom', 'mithu', 'shawn', 'takrim', 'amirul', 'emon'],
+    friendsAge: [21, 22, 23, 24, 25, 26]
 };
 
 const userJSON = JSON.stringify(user);
